4c5e76b2e9013fa8ff7ea2b17dd7a938e397a6d8
The_Lemur <pagangodsncaviar@gmail.com>
2025-07-06 22:49:16 -0500

    added astroids, bullets, and MC

--- main.py
+++ main.py
@@ -1,19 +1,53 @@
 import pygame
 from constants import *
+from circleshape import *
+from player import *
+from asteroid import *
+from asteroidfield import *
+from shot import *
+
 
 def main():
-    pygame.init
+    pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock() 
+    dt = 0
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
+    
+    Asteroid.containers = (asteroids, updatable, drawable)
+    Player.containers = (updatable, drawable)
+    AsteroidField.containers = (updatable,)
+    Shot.containers = (shots, updatable, drawable)
+    
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    
+    asteroid_field = AsteroidField()
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        for drawing in drawable:
+            drawing.draw(screen)
+
+        updatable.update(dt)
+
+        for asteroid in asteroids:
+            if player.collision(asteroid):
+                sys.exit()
+
         pygame.display.flip()
+        
+        dt = clock.tick(60)/1000
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+    print(asteroids)
 
 
 if __name__ == "__main__":
---
+++ shot.py
@@ -0,0 +1,15 @@
+import pygame
+from circleshape import CircleShape
+
+
+class Shot(CircleShape):
+    def __init__(self, x, y, radius):
+        super().__init__(x, y, radius)
+
+        self.velocity = pygame.Vector2(0, 1)
+
+    def draw(self, screen):
+        pygame.draw.circle(screen, "White", self.position, self.radius, width=2)
+
+    def update(self, dt):
+        self.position += (self.velocity * dt)
